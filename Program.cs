43bcb1333285e2d7c94b1ba0fac65408dec3844e
brstevenn <brayangaitan81@gmail.com>
2024-02-22 22:53:35 -0500

    Change port

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using SocketSignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://localhost:5101/");
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
